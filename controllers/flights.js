705d2be55e00dc9c6019785e7fea54d09d76c6fe
Looloo414 <alexafaulkison@gmail.com>
2020-12-03 20:28:13 -0700

    Adding ticket route

--- controllers/flights.js
+++ controllers/flights.js
@@ -3,12 +3,13 @@ const Flight = require('../models/flight')
 module.exports = {
     index,
     create,
-    new: newFlight
+    new: newFlight,
+    show
 }
 
 function index(req, res) {
     Flight.find({}, function(err, flights) {
-        res.render('flights/index', {airlines: "All flight", flights: flights})
+        res.render('flights/index', {airline: "All flights", flights: flights})
     })
 }
 
@@ -17,14 +18,23 @@ function newFlight(req, res) {
 }
 
 function create(req, res) {
+    for (let key in req.body) {
+        if (req.body[key] === '') delete req.body[key]
+    }
     const flight = new Flight(req.body)
     flight.save(function(err) {
         if(err) {
             console.log(err)
-            return res.render('flights/new', {err: err})
+            return res.render('flights/new', {err: err, airline: 'All Flights'})
         }
         console.log(flight)
         res.redirect('/flights')
     })
 
+}
+
+function show(req, res) {
+    Flight.findById(req.params.id, function(err, flight) {
+        res.render('flights/show', {airline: 'Flight Details', flight})
+    })
 }
--- models/flight.js
+++ models/flight.js
@@ -1,15 +1,21 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
+const ticketSchema = new Schema ({
+    seat: {type: String, match: /[A-F][1-9]\d?/},
+    price: {type: Number, min: 0}
+
+})
+
 const flightSchema = new Schema ({
     airline: {type: String, enum:['American', 'Southwest', 'United']},
     airport: {type: String, enum:['AUS', 'DFW', 'DEN', 'LAX', 'SAN'], default: 'DEN'},
     flightNo: {type: Number, min: 10, max: 9999},
     departs: {
         type: Date,
-        default: function() {
-        return new Date().getFullYear()
-      }}
+        default:() => Date.now() + 365*24*60*60*1000
+      },
+    tickets: [ticketSchema]
 })
 
 module.exports = mongoose.model('Flight', flightSchema)
